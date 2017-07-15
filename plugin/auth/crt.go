b54d28223f789a2d65255676800d0ff3b3a4aa3a
Mariusz Gronczewski <xani666@gmail.com>
2017-07-15 16:23:03 +0200

    add test data, cert test, move to glide

---
+++ plugin/auth/crt.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"crypto/x509"
+//	"encoding/pem"
+	"io/ioutil"
+	"fmt"
+)
+
+type Auth struct {
+	root *x509.CertPool
+}
+
+func New() *Auth {
+	var a Auth
+	return &a
+}
+
+func (a *Auth)LoadCA(file string)(err error) {
+	a.root = x509.NewCertPool()
+	out, err := ioutil.ReadFile(file)
+	if err != nil {return}
+	ok := a.root.AppendCertsFromPEM([]byte(out))
+	if !ok {return fmt.Errorf("Can't load cert from %s",file)}
+	return
+}
